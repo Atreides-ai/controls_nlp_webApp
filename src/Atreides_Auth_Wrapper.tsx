00b209835d17afb9537dce7e45cbb888f219a1f2
pariah140 <richard.hurley6@gmail.com>
2020-03-18 11:05:16 +0000

    working on sign in? git

--- src/Atreides_Auth_Wrapper.tsx
+++ src/Atreides_Auth_Wrapper.tsx
@@ -1,55 +1,47 @@
-import React, { useState } from "react";
-import {
-  RequireNewPassword,
-  ConfirmSignIn,
-  ForgotPassword,
-  TOTPSetup,
-  Authenticator,
-  VerifyContact
-} from "aws-amplify-react";
-import CustomSignIn from "./customSignIn.js";
-import Box from "@material-ui/core/Box";
-import { Redirect } from "react-router-dom";
-import awsmobile from "./aws-exports";
-import useStyles from "./useStyles";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
+import React, { useState } from 'react';
+import Box from '@material-ui/core/Box';
+import { Redirect } from 'react-router-dom';
+import useStyles from './useStyles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import AtreidesSignIn from 'Atreides_Sign_In';
 
-export default function AuthComponent({ appCallback }) {
-  const classes = useStyles();
-  const [authState, SetAuthState] = useState(false);
+export default function AuthComponent(appCallback: any): JSX.Element {
+    const classes = useStyles();
+    const [authStage, setAuthStage] = useState<string>('');
+    const [authState, SetAuthState] = useState(false);
 
-  const handleStateChange = state => {
-    if (state === "signedIn") {
-      appCallback(true);
-      SetAuthState(true);
-    } else {
-    }
-  };
+    const handleStateChange = (state: string): void => {
+        if (state === 'signedIn') {
+            appCallback(true);
+            SetAuthState(true);
+        } else {
+        }
+    };
 
-  return (
-    <Box className={classes.loginImage}>
-      {authState === true && <Redirect to="/submitFile" />}
-      {authState === false && (
-        <Grid
-          container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          justify="center"
-          style={{ minHeight: "100vh" }}
-        >
-          <Grid item xs={3}>
-            <Paper elevation={3} className={classes.loginSurface}>
-              <AtreidesSignIn></AtreidesSignIn>
-              <AtreidesMFA></AtreidesMFA>
+    return (
+        <Box className={classes.loginImage}>
+            {authState === true && <Redirect to="/submitFile" />}
+            {authState === false && (
+                <Grid
+                    container
+                    spacing={0}
+                    direction="column"
+                    alignItems="center"
+                    justify="center"
+                    style={{ minHeight: '100vh' }}
+                >
+                    <Grid item xs={3}>
+                        <Paper elevation={3} className={classes.loginSurface}>
+                            <AtreidesSignIn></AtreidesSignIn>
+                            {/* <AtreidesMFA></AtreidesMFA>
               <AtreidesMFASetUp></AtreidesMFASetUp>
               <AtreidesForgotPassword></AtreidesForgotPassword>
-              <AtreidesReqNewPassword></AtreidesReqNewPassword>
-            </Paper>
-          </Grid>
-        </Grid>
-      )}
-    </Box>
-  );
+              <AtreidesReqNewPassword></AtreidesReqNewPassword> */}
+                        </Paper>
+                    </Grid>
+                </Grid>
+            )}
+        </Box>
+    );
 }
--- src/Atreides_Sign_In.tsx
+++ src/Atreides_Sign_In.tsx
@@ -1,85 +1,71 @@
-import React, { useState } from "react";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
-import Container from "@material-ui/core/Container";
-import useStyles from "./useStyles";
-import {useTheme, Theme} from "@material-ui/core/styles"
-import {Auth} from 'aws-amplify'
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
+import useStyles from './useStyles';
+import { useTheme, Theme } from '@material-ui/core/styles';
+import { Auth } from 'aws-amplify';
 
-export default function AtreidesSignIn() {
-  const theme = useTheme<Theme>();
-  const classes = useStyles(theme);
+export default function AtreidesSignIn(): JSX.Element {
+    const theme = useTheme<Theme>();
+    const classes = useStyles(theme);
 
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
 
-  const setEmailInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value);
-  }
+    const setEmailInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        setEmail(event.target.value);
+    };
 
-  const setPasswordInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  }
+    const setPasswordInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        setPassword(event.target.value);
+    };
 
-  const signIn = () => {
-    Auth.signIn(email, password)
-  }
+    const signIn = (): void => {
+        Auth.signIn(email, password);
+    };
 
     return (
         <Container component="main" maxWidth="xs">
-          <div className={classes.login}>
-            <Typography
-              component="h1"
-              variant="h5"
-              align="center"
-              color="primary"
-            >
-              Sign in to Atreides
-            </Typography>
-            <div className={classes.muiform}>
-              <form className={classes.muiform} noValidate>
-                <TextField
-                  style={{ backgroundColor: "white" }}
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="username"
-                  name="username"
-                  autoComplete="Email Address"
-                  onChange={setEmailInput}
-                />
-                <TextField
-                  style={{ backgroundColor: "white" }}
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                  onChange={setPasswordInput}
-                />
-                <Button
-                  variant="contained"
-                  color="primary"
-                  className={classes.muisubmit}
-                  onClick={signIn}>
-                  Sign In
-                </Button>
-                <Button
-                  variant="contained"
-                  className={classes.muisubmit}
-                  onClick={}
-                >
-                  Forgot Password?
-                </Button>
-              </form>
+            <div className={classes.login}>
+                <Typography component="h1" variant="h5" align="center" color="primary">
+                    Sign in to Atreides
+                </Typography>
+                <div className={classes.muiform}>
+                    <form className={classes.muiform} noValidate>
+                        <TextField
+                            style={{ backgroundColor: 'white' }}
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            name="username"
+                            autoComplete="Email Address"
+                            onChange={setEmailInput}
+                        />
+                        <TextField
+                            style={{ backgroundColor: 'white' }}
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            onChange={setPasswordInput}
+                        />
+                        <Button variant="contained" color="primary" className={classes.muisubmit} onClick={signIn}>
+                            Sign In
+                        </Button>
+                        <Button variant="contained" className={classes.muisubmit}>
+                            Forgot Password?
+                        </Button>
+                    </form>
+                </div>
             </div>
-          </div>
         </Container>
     );
-  }
 }
