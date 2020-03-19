e3fcf578a9016e0ed49579f776beccb0488f383a
pariah140 <richard.hurley6@gmail.com>
2020-03-19 14:43:18 +0000

    fixed eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,8 +13,8 @@ module.exports = {
         },
     },
     rules: {
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn"
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
     },
     settings: {
         react: {
