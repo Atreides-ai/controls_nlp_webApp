a984cdd779717ab74e1526b214bb5bce37aa308e
pariah140 <richard.hurley6@gmail.com>
2020-04-02 08:49:29 +0100

    updated pilot and demos to 50 controls

--- src/submit_file.js
+++ src/submit_file.js
@@ -46,7 +46,7 @@ export default function SubmitFile({ user }) {
 
     const reduceFileForPilot = fileString => {
         const parsedFile = d3.csvParse(fileString);
-        const sample = parsedFile.slice(0, 16);
+        const sample = parsedFile.slice(0, 50);
 
         return d3.csvFormat(sample);
     };
