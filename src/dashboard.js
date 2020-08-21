e2c883e8a7123a0c71183ca74552289aa6238e21
pariah140 <richard.hurley6@gmail.com>
2020-08-21 09:59:08 +0100

    got an idea

--- src/dashboard.js
+++ src/dashboard.js
@@ -30,48 +30,12 @@ import BugReportIcon from '@material-ui/icons/BugReport';
 import PieChartCard from './PieChartCard';
 import MaterialTable, { Column } from 'material-table';
 import DataTablePopUp from './DataTablePopUp';
-import { forwardRef } from 'react';
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowUpward from '@material-ui/icons/ArrowUpward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
 import PrintButton from 'PDF_Button';
 import dashboardDescriptions from 'Dashboard_Descriptions';
 
 Storage.configure({ level: 'private' });
 
 export default function Dashboard() {
-    const tableIcons = {
-        Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-        Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-        Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-        DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-        Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-        Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-        Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-        FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-        LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-        ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-        Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-        SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />),
-        ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-        ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
-    };
-
     useEffect(() => {
         getFile();
     }, []);
@@ -277,22 +241,7 @@ export default function Dashboard() {
                             </Typography>
                         </Grid>
                         <Grid item xs={12} sm={2} md={2} lg={2}>
-                            <DataTablePopUp
-                                table={
-                                    <MaterialTable
-                                        options={{
-                                            exportButton: true,
-                                        }}
-                                        icons={tableIcons}
-                                        columns={[
-                                            { title: 'Control Description', field: 'Control Description' },
-                                            { title: 'Overall Score', field: 'control_summary_rating' },
-                                        ]}
-                                        data={dashboardfile}
-                                        title="Analysis Summary"
-                                    />
-                                }
-                            />
+                        <DataTablePopUp analysisField="control_summary_rating" dashboardFile={dashboardfile} />
                         </Grid>
                     </Grid>
                     <Grid container direction="row" spacing={3} justify="center">
@@ -345,23 +294,7 @@ export default function Dashboard() {
                             </Typography>
                         </Grid>
                         <Grid item xs={12} sm={2} md={2} lg={2}>
-                            <DataTablePopUp
-                                table={
-                                    <MaterialTable
-                                        options={{
-                                            exportButton: true,
-                                        }}
-                                        icons={tableIcons}
-                                        columns={[
-                                            { title: 'Control Description', field: 'Control Description' },
-                                            { title: 'Risk Description', field: 'Risk Description' },
-                                            { title: 'Control Relevance To Risk', field: 'control_relevance_to_risk' },
-                                        ]}
-                                        data={dashboardfile}
-                                        title="Analysis Summary"
-                                    />
-                                }
-                            />
+                            <DataTablePopUp analysisField="control_relevance_to_risk" dashboardFile={dashboardfile} />
                         </Grid>
                     </Grid>
                     <Grid container direction="row" spacing={1}>
