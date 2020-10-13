86a00af515224e21099aec5b3f780f6629376995
pariah140 <richard.hurley6@gmail.com>
2020-10-13 14:33:28 +0100

    working on filebrowser

---
+++ src/FileBrowser.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { DataGrid, ColDef } from '@material-ui/data-grid';
+
+const FileBrowser = (props: { apiKey: string; token: string }): JSX.Element => {
+    const groupedData = useState<Array<Array<object>>>();
+    const columns: ColDef[] = [
+        { field: 'id', headerName: 'ID', width: 70 },
+        { field: 'filename', headerName: 'File', width: 130 },
+        { field: 'created', headerName: 'Created', width: 130 },
+        { field: 'dashboard', headerName: 'Download', width: 130 },
+        { field: 'dashboard', headerName: 'Dashboard', width: 130 },
+    ];
+
+    const getAllControls = () => {
+        // TODO make some api request to the get all controls endpoint
+    };
+
+    const groupByJobId = () => {
+        // TODO group the controls in the following data structure [[{meta: {control}s}], ...]
+        // input will be [{}] so array.filter by obj ['job_id']
+    };
+
+    const getTableRows = () => {
+        // each grouped object will have a meta header to match row format required
+    };
+
+    return <DataGrid rows={rows} columns={columns} pageSize={25} checkboxSelection />;
+};
