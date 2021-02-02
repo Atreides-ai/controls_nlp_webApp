fa3e532ef3da2936e20d415317ee45442a6fb5db
pariah140 <richard.hurley6@gmail.com>
2021-02-02 16:42:19 +0000

    hotfix

--- src/Dashboard_Descriptions.js
+++ src/Dashboard_Descriptions.js
@@ -17,23 +17,23 @@ const dashboardDescriptions = {
         'These controls have a fair relevance rating to the risk to which they are linked. This means that there is some relation between the risk and control but it could be clearer, these controls may need revision.',
     poor_risk_card:
         'These controls have a poor relevance rating to the risk to which they are linked. These risks and controls will require review. Look out for poorly worded risks that do not have a clear cause, event and impact, or controls that may be have been linked by mistake',
-    automated_manual_pie:
-        'This chart shows the split of conrols that were automated or manual. If no data was provided in the input this chart will show no data provided.',
-    contains_what_pie:
+    contains_whats_pie:
         'A what is defined as a clear statement indicating the core action(s) being performed. It is rare that a control does not include this element. However, if your controls do not, be careful that the control description is not a lengthy process description which does not include a clear description of action(s). ',
-    contains_how_pie:
+    contains_hows_pie:
         'A how is defined as subordinate statement to the what, defining how the action is being done or how it is evidenced as being done. If your controls do not contain a how it is likely that the what stops short of any description of the steps taken, or reference to the policy that describes the steps required.',
-    contains_who_pie:
+    contains_whos_pie:
         'A who is defined as people (named or by role) that are performing actions in the control. It is important that the control has a clear operator, watch out for generic identifiers such as Management, where accountability is difficult to establish.',
     contains_whens_pie:
         'A when is defined as an indication of the time when things in the control happen. This is commonly known as the frequency. Watch out for conflicting frequencies or ambiguity.',
-    multiple_what_pie:
+    contains_thresholds_pie:
+        'A threshold is numerical trigger that indicates a review or action is required. These indicate that the control is a Management Review Control or MRC.',
+    contains_multiple_whats_pie:
         'There is a risk that the controls you write are too long and turn into process descriptions. Having multiple whats in a control is an indication that this has happened and that the control might need to be broken up into multiple smaller controls.',
-    multiple_how_pie:
+    contains_multiple_hows_pie:
         'There is a risk that the controls you write are too long and turn into process descriptions. Similar to Whats, having multiple hows in a control is an indication that this has happened and that the control might need to be broken up into multiple smaller controls.',
-    multiple_when_pie:
+    contains_multiple_whens_pie:
         'There is a risk that the controls you write have many named frequencies. Be careful. Ask yourself, is it clear when this control occurs? What triggers it?',
-    multiple_who_pie:
+    contains_multiple_whos_pie:
         'There is a risk that if many people are named or identified in a control procedure that accountability is blurred. Ask yourself, do we know who is actually doing the control from the description?',
 };
 
